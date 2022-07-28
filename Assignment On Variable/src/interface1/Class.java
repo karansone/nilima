60522869142ed8f9b4bd06134e2b2b5b0e7df750
karansone <110161905+karansone@users.noreply.github.com>
2022-07-28 14:26:44 +0530

    Update Class.java

--- Assignment On Variable/src/interface1/Class.java
+++ Assignment On Variable/src/interface1/Class.java
@@ -7,18 +7,18 @@ public void D1(){
 public void E1(){
 	System.out.println("running E2");
 }
-public void F3(){
+public void N3(){
 	System.out.println("running F3");
 }
-public void G4(){
-	System.out.println("running G4");
+public void N4(){
+	System.out.println("running N");
 }
 public static void main(String[] args) {
 	Class x= new Class();
 	x.D1();
 	x.E1();
-	x.F3();
-	x.G4();
+	x.N3();
+	x.N4();
 }
 
 
